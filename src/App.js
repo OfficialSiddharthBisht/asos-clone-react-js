d6766ab20624499f397d54bfcd230fb92ee5a6e8
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-29 01:32:07 +0530

    added bergpage siddharth and select

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Men/home';
 import Footer  from './components/Footer/footer';
+import {Bergpage} from './components/bergpage/bergpage'
 
 function App() {
   return (
     <div className="App">
       <>
-        <h1>ASOS Clone</h1>
         <Home></Home>
+        {/* <Bergpage /> */}
         <Footer/>
       </>
     </div>
