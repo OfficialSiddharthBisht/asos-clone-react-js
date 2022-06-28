aa57e5f6541a63355e967b6e3bbaa2ccb3868b3a
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-29 01:06:53 +0530

    made and pushed the footer siddharth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Men/home';
+import Footer  from './components/Footer/footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <>
         <h1>ASOS Clone</h1>
         <Home></Home>
+        <Footer/>
       </>
     </div>
   );
