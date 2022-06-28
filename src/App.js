a25c33c02c03f6b25e69e3535c034a2f12a9c5e1
AnkitAnkushe <ankitankushe@gmail.com>
2022-06-28 14:24:22 +0530

    added women Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Men/home';
 
 function App() {
   return (
     <div className="App">
       <>
         <h1>ASOS Clone</h1>
+        <Home></Home>
       </>
     </div>
   );
